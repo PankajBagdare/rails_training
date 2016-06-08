eab8655379bd44fe7511bc4d0778dba93a7f3b8e
PankajBagdare <pbagdare@grepruby.com>
2016-06-08 17:43:40 +0530

    frequency-array

--- day1/arrcount.rb
+++ day1/arrcount.rb
@@ -1,15 +1,10 @@
 
 user_input = [1,5,4,6,5,4,4,7,5,5,4,5,2,6]
 
-hash = Hash.new
+hash = Hash.new(0)
 
-
-
-hash.each do |key,value|
-  user_input.each do |input_element| 
-    key = user_input
-    value += 1
-  end
+user_input.each do |element|
+  hash[element] +=1
 end
 
 puts hash

c757a70b4f47dbe6571e99fd2c97a72a9267d8c6
PankajBagdare <pbagdare@grepruby.com>
2016-06-10 16:22:45 +0530

    Update salestax program

--- day2/salestax.rb
+++ day2/salestax.rb
@@ -2,7 +2,7 @@ require 'io/console'
 $sales_tax_list=[]
 $final_price_list=[]
 $total_sales_tax = 0 
-$total_price = 0
+
 
 # method to calculate price and tax
 
@@ -10,12 +10,13 @@ def calculate_price(line)
    splited_input = line.split
    price = splited_input.last
    newprice = price.to_f
-
+   sales_tax=0
+   import_tax=0
    if line.include?("music"||"perfume")
-     sales_tax = newprice/10
+     sales_tax = (newprice/10).round(2)
    end
    if line.include?("book"||"chocolate"||"piils")
-     import_tax = newprice/20
+     import_tax = (newprice/20).round(2)
    end
    if line.include?("imported")
      @sales_tax = sales_tax + import_tax
@@ -23,8 +24,8 @@ def calculate_price(line)
      @sales_tax = sales_tax
    end
    @final_price = newprice + @sales_tax
-    $sales_tax_list.push($sales_tax)
-   return @final_price, @sales_tax
+   @final_price=@final_price.round(2)
+    $sales_tax_list.push(@sales_tax)
 end 
 
 # printing the output line by line
@@ -33,36 +34,48 @@ def output(line)
   splited = line.split
   calculate_price(line)
   len = splited.length
-  for i in 1...len
-    print "#{splited[i]}"
+  for i in 0...len-2
+    print "#{splited[i]} "
   end
-  print " : #{@final_price} \n"
+  print ": #{@final_price} \n"
   $final_price_list.push(@final_price)
-
 end
 
 # to compute total and print
 
 def compute_total_and_print()
+  @total_price=0
   $final_price_list.each do |element|     
-    $total_price = $total_price + element
+    @total_price = @total_price + element
   end
+  @total_sales_tax=0
   $sales_tax_list.each do |element|
-    $total_sales_tax = $total_sales_tax + element
+    @total_sales_tax = @total_sales_tax + element
   end
-  puts "Sales Tax : #{$total_sales_tax}"
-  puts "Total : #{total_price}"
+  puts "Sales Tax : #{@total_sales_tax.round(2)}"
+  puts "Total : #{@total_price.round(2)}"
 end
+
 
 # Main program starts from here
 
-#puts "Enter the details of what is purchased"
+# puts "Enter the details of what is purchased"
+# user_enters = gets.chomp
+# tempfile = File.new("input.txt","w")
 
-input = File.open('input1.txt').read
+# tempfile.puts(user_enters)
+#  input = tempfile.read
+puts "Enter any one name of file"
+puts "input1.txt"
+puts "input2.txt"
+puts "input3.txt"
+enter = gets.chomp
+input = File.open(enter).read
 
 input.each_line do |line| 
   output(line)
 end
 
 compute_total_and_print
+
 
